f4233a3d91bdd7496025375531c982b553bb01f2
Julen <julenuni@gmail.com>
2020-08-06 18:19:00 +0200

    Initial Commit

---
+++ dBus redesign/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dBus redesign
+//
+//  Created by Julen Miner on 06/08/2020.
+//  Copyright © 2020 Julen Miner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
